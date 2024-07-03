cd3dc06bada86c2793a35755cd0aa65679c7f44c
Pedro Alegre <alegre.p@gmail.com>
2024-07-03 21:55:09 +0200

    First commit

---
+++ src/test/java/com/springbootcourse/springbootdemoapp/SpringbootdemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcourse.springbootdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
